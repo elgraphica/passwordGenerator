50b9fea10cc70f8aac6e24a79d7327043558fcdb
elgraphica <elferenczi@outlook.com>
2023-01-26 21:35:18 +0000

    updated Function to prompt user for password options.. updated Function to generate password with user input and put a comment for each step

--- script.js
+++ script.js
@@ -90,46 +90,96 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  var length = prompt("Enter the desired length of the password (between 8 and 128 characters):");
+  // Prompt the user for the password length
+  const length = parseInt(prompt("How many characters do you want your password to contain?"));
 
-  // Validate length
-  while(length < 8 || length > 128) {
-    alert("Password length must be between 8 and 128 characters. Please try again.");
-    length = prompt("Enter the desired length of the password (between 8 and 128 characters):");
+  // Check if the input is a number
+  if (isNaN(length)) {
+    alert("Please enter a valid number for the password length.");
+    return;
   }
 
-  var hasLower = confirm("Include lowercase characters in the password?");
-  var hasUpper = confirm("Include uppercase characters in the password?");
-  var hasNumeric = confirm("Include numeric characters in the password?");
-  var hasSpecial = confirm("Include special characters in the password?");
-
-  // Validate at least one character type is selected
-  while(!hasLower && !hasUpper && !hasNumeric && !hasSpecial) {
-    alert("At least one character type must be selected. Please try again.");
-    hasLower = confirm("Include lowercase characters in the password?");
-    hasUpper = confirm("Include uppercase characters in the password?");
-    hasNumeric = confirm("Include numeric characters in the password?");
-    hasSpecial = confirm("Include special characters in the password?");
+  // Check if the length is at least 8 characters and no more than 128 characters
+  if (length < 8 || length > 128) {
+    alert("The password length must be between 8 and 128 characters.");
+    return;
   }
 
+  // Confirm whether the user wants to use special characters
+  const useSpecialCharacters = confirm("Do you want to use special characters in your password?");
+
+  // Confirm whether the user wants to use numeric characters
+  const useNumbers = confirm("Do you want to use numbers in your password?");
+
+  // Confirm whether the user wants to use lowercase letters
+  const useLowercaseLetters = confirm("Do you want to use lowercase letters in your password?");
+
+  // Confirm whether the user wants to use uppercase letters
+  const useUppercaseLetters = confirm("Do you want to use uppercase letters in your password?");
+
+  // Return the password options as an object
   return {
-    length: length,
-    hasLower: hasLower,
-    hasUpper: hasUpper,
-    hasNumeric: hasNumeric,
-    hasSpecial: hasSpecial
-  }
+    length,
+    useSpecialCharacters,
+    useNumbers,
+    useLowercaseLetters,
+    useUppercaseLetters
+  };
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-  var randomIndex = Math.floor(Math.random() * arr.length);
+  // Get a random index from the array
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  // Return the element at the random index
   return arr[randomIndex];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  // Get the password options
+  const options = getPasswordOptions();
 
+  // Store the password as an array of characters
+  let password = [];
+
+  // Store the selected character types
+  let selectedCharacters = [];
+
+  // If the user wants to use special characters, add them to the selected characters
+  if (options.useSpecialCharacters) {
+    selectedCharacters = selectedCharacters.concat(specialCharacters);
+  }
+
+  // If the user wants to use numeric characters, add them to the selected characters
+  if (options.useNumbers) {
+    selectedCharacters = selectedCharacters.concat(numericCharacters);
+  }
+
+  // If the user wants to use lowercase letters, add them to the selected characters
+  if (options.useLowercaseLetters) {
+    selectedCharacters = selectedCharacters.concat(lowerCasedCharacters);
+  }
+
+  // If the user wants to use uppercase letters, add them to the selected characters
+  if (options.useUppercaseLetters) {
+    selectedCharacters = selectedCharacters.concat(upperCasedCharacters);
+  }
+
+  // If the user does not select any character types, return an error message
+  if (selectedCharacters.length === 0) {
+    alert("Please select at least one character type to include in the password.");
+    return;
+  }
+
+  // Generate the password by selecting a random character from the selected characters for the length of the password
+  for (let i = 0; i < options.length; i++) {
+    password.push(getRandom(selectedCharacters));
+  }
+
+  // Convert the password array to a string and return it
+  return password.join("");
 }
 
 // Get references to the #generate element
